6ed5972d8193ef2f249fb88027aad972765f37e4
wiggins <vierhang@163.com>
2023-02-12 16:02:09 +0800

    job api

--- master/main/main.go
+++ master/main/main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/vierhang/crontab/master"
 )
+
+var (
+	configFile string
+)
+
+func initArgs() {
+	flag.StringVar(&configFile, "config", "./master.json", "传入指定 master.json 路径")
+	flag.Parse()
+}
 
 func main() {
 	var (
 		err error
 	)
+	initArgs()
+	if err = master.InitConfig(configFile); err != nil {
+		panic(err)
+	}
+
+	if err = master.InitJobMgr(); err != nil {
+		panic(err)
+	}
+
 	if err = master.InitApiServer(); err != nil {
 		panic(err)
 	}
 
+	fmt.Println("start success")
 }
